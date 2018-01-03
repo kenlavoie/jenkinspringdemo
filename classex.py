74c65c3083276b49622f417c2fa872a9df016d47
Ken Lavoie <ken.lavoie@docker.com>
2018-01-03 10:30:24 -0500

    deleted:    classex.py 	deleted:    classtest.py 	deleted:    containerlist.py 	deleted:    djangodock 	deleted:    djangodock.zip 	deleted:    dockerstuff.py 	deleted:    flaskclass/.DS_Store 	deleted:    flaskclass/Dockerfile 	deleted:    flaskclass/api.py 	deleted:    flaskclass/entrypoint.sh 	deleted:    flaskclass/requirements.txt 	deleted:    kenhttprm.rtf 	deleted:    playground.js 	deleted:    pyflaskapi/.DS_Store 	deleted:    pyflaskapi/Dockerfile 	deleted:    pyflaskapi/api.py 	deleted:    pyflaskapi/entrypoint.sh 	deleted:    pyflaskapi/requirements.txt 	deleted:    requirements.txt

--- classex.py
+++
@@ -1,9 +0,0 @@
-from classtest import *
-
-firstname = CustomerFirstName("Ken")
-
-lastname = CustomerLastName("Loverboy")
-
-#print(firstname)
-#print(lastname)
-print(firstname, lastname)
--- playground.js
+++
@@ -1,9 +0,0 @@
-var coding = {
-    thought: 'an asshole'
-    person: 'Fred is',
-    pudding: function() {
-        console.log('Hello World!')
-    }
-}
-coding.pudding();
-console.log(coding.person + coding.thought )
